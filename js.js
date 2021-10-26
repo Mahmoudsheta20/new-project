252854f45e1b6a7b5809d9e2043d14ce8b5da21c
Mahmoudsheta20 <87619966+Mahmoudsheta20@users.noreply.github.com>
2021-10-26 18:00:13 +0200

    Initial commit

---
+++ js.js
@@ -0,0 +1,9 @@
+let other = document.querySelector('.other');
+let menu = document.querySelector('.header .mega-menu');
+
+
+
+other.addEventListener('click', (e) => {
+
+    menu.classList.toggle('activ')
+});
